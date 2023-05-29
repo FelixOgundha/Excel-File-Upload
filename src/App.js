d4af65bc5a519207e537ae2d49d83fbb298f4bc7
FelixOgundha <fogundha@strathmore.edu>
2023-05-29 22:31:37 +0300

    first ccommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import React from 'react';
+import { Form } from 'react-bootstrap';
 
 function App() {
+  const [selectedFile, setSelectedFile] = React.useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      // Create a FormData object to send the file
+      const formData = new FormData();
+      formData.append('departmentList', selectedFile);
+
+      // Perform the upload using your preferred method (e.g., Axios)
+      // Replace `uploadUrl` with the appropriate URL for your server-side upload endpoint
+      const uploadUrl = 'https://api-meru.ngamia.africa/api/CourseManagement/UploadDepartmentsCSV';
+      axios.post(uploadUrl, formData)
+        .then(response => {
+          // Handle the successful upload response
+          console.log('Upload success:', response);
+        })
+        .catch(error => {
+          // Handle upload errors
+          console.error('Upload error:', error);
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Upload CSV File</h2>
+        <input type="file" accept=".csv" onChange={handleFileChange} />
+        <button onClick={handleUpload}>Upload</button>
+      </div>
     </div>
   );
 }
